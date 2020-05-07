32af7d9a1a1c872dbb909c1c409f68854e777cd9
alien-sim <simrajput555@gmail.com>
2020-05-07 12:13:49 +0530

    simple amazon scarpping

---
+++ amz_scrap.py
@@ -0,0 +1,67 @@
+import os
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+import csv
+import pandas as pd
+
+
+chrome_options = Options()
+# chrome_options.add_argument(" - incognito ")
+chrome_options.add_argument("--headless")
+driver_path = os.path.join(os.getcwd(), 'chromedriver.exe')
+# driver = webdriver.Chrome(driver_path, options=chrome_options)
+driver = webdriver.Chrome(executable_path="chromedriver.exe",options=chrome_options)
+driver.get('https://www.amazon.in')
+
+time.sleep(2)
+search = driver.find_element_by_id('twotabsearchtextbox')
+search.send_keys('all mobiles under Rs10000')
+
+search2 = driver.find_element_by_class_name('nav-input')
+search2.click()
+time.sleep(2)
+
+page_list = driver.find_elements_by_class_name('a-disabled')
+pages = int(page_list[-1].text)
+
+
+def getdata(driver,pages):
+    page_html = driver.page_source
+    soup = BeautifulSoup(page_html, 'html.parser')
+    current = 0
+    data = {}
+    df = pd.DataFrame(columns=['Title', 'Price', 'Star'])
+    dictionary = {'One':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5}
+    while current<pages:
+        all_divs = soup.findAll('div', attrs={'class':'sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28'})
+        for mobiles in all_divs:
+            title = mobiles.find('span', attrs={'class':'a-size-medium a-color-base a-text-normal'})
+            price = mobiles.find('span', attrs={'class':"a-price-whole"})
+            rating = mobiles.find('span', attrs={'class':'a-icon-alt'})
+            try:
+                data['Title'] = title.text
+            except:
+                data['Title'] = '--'
+
+            try:
+                data['Price'] = price.text
+            except:
+                data['Price'] = '-'
+            
+            try:
+                data['Star'] = rating.text
+            except:
+                data['Star'] = '--'
+                
+            df = df.append(data, ignore_index=True)
+        current+=1
+        next_page = driver.find_element_by_class_name('a-last')
+        next_page.click()
+    return df
+    
+
+list_data = getdata(driver,pages)
+list_data.to_csv('tests2.csv')
